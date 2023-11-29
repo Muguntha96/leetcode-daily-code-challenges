584d878e10d2ca1144124d775fbab3461c048cd3
Muguntha96 <muguntharamkumar1996@gmail.com>
2023-11-29 11:02:05 -0400

    complete exercise 7

--- challenges.js
+++ challenges.js
@@ -168,3 +168,23 @@ function countMatches(items,ruleKey,ruleValue){
   return count
 }
 console.log(countMatches([["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]],"color","silver"))
+
+// Exercise:7 Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+// Example 1:
+
+// Input: haystack = "sadbutsad", needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// The first occurrence is at index 0, so we return 0.
+// Example 2:
+
+// Input: haystack = "leetcode", needle = "leeto"
+// Output: -1
+// Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+function firstOccurence(needle,haystack){
+  let indexOfFirst = haystack.indexOf(needle)
+  return indexOfFirst
+}
+console.log(firstOccurence("sad","sadbutsad"))
+console.log(firstOccurence("leetcode","leeto"))
