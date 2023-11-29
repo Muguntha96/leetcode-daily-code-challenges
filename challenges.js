2919b1282502a259e777617a7ddb05a7faa01181
Muguntha96 <muguntharamkumar1996@gmail.com>
2023-11-29 10:51:13 -0400

    complete leetcode exercise 6

--- challenges.js
+++ challenges.js
@@ -132,4 +132,39 @@ function shuffleString(s,indices){
   return newArr.join("")
   }
   console.log(shuffleString("codeleet",[4,5,6,7,0,2,1,3]))
-  
+
+// Exercise 6:You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
+// The ith item is said to match the rule if one of the following is true:
+
+// ruleKey == "type" and ruleValue == typei.
+// ruleKey == "color" and ruleValue == colori.
+// ruleKey == "name" and ruleValue == namei.
+// Return the number of items that match the given rule.
+
+// Example 1:
+// Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
+// Output: 1
+// Explanation: There is only one item matching the given rule, which is ["computer","silver","lenovo"].
+
+// Example 2:
+// Input: items = [["phone","blue","pixel"],["computer","silver","phone"],["phone","gold","iphone"]], ruleKey = "type", ruleValue = "phone"
+// Output: 2
+// Explanation: There are only two items matching the given rule, which are ["phone","blue","pixel"] and ["phone","gold","iphone"]. Note that the item ["computer","silver","phone"] does not match.
+function countMatches(items,ruleKey,ruleValue){
+  let count =0
+  let indexValue
+  if(ruleKey ==="type"){
+    indexValue=0
+  }else if(ruleKey ==="color"){
+    indexValue=1
+  }else{
+    indexValue=2
+  }
+  items.forEach(item => {
+    if(item[indexValue]===ruleValue){
+      count++
+    }
+  });
+  return count
+}
+console.log(countMatches([["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]],"color","silver"))
