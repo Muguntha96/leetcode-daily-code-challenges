60d68683f3c5e7490c5770efa94d18fc6b4faff4
muguntha <muguntharamkumar1996@gmail.com>
2023-12-22 04:51:55 -0400

    solve leet code difference sum problem

--- challenges.js
+++ challenges.js
@@ -823,3 +823,43 @@ return outputArr.join(" ")
 console.log(truncateSentence("Hello how are you Contestant",4))
 console.log(truncateSentence("What is the solution to this problem",4))
 console.log(truncateSentence("chopper is not a tanuki",5))
+
+// Exercise:24 You are given a positive integer array nums.
+
+// The element sum is the sum of all the elements in nums.
+// The digit sum is the sum of all the digits (not necessarily distinct) that appear in nums.
+// Return the absolute difference between the element sum and digit sum of nums.
+
+// Note that the absolute difference between two integers x and y is defined as |x - y|.
+// Example 1:
+
+// Input: nums = [1,15,6,3]
+// Output: 9
+// Explanation: 
+// The element sum of nums is 1 + 15 + 6 + 3 = 25.
+// The digit sum of nums is 1 + 1 + 5 + 6 + 3 = 16.
+// The absolute difference between the element sum and digit sum is |25 - 16| = 9.
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: 0
+// Explanation:
+// The element sum of nums is 1 + 2 + 3 + 4 = 10.
+// The digit sum of nums is 1 + 2 + 3 + 4 = 10.
+// The absolute difference between the element sum and digit sum is |10 - 10| = 0.
+
+function differenceOfSum(nums){
+  let sumNums=0
+  let sumDigits=0
+  nums.forEach(element => {
+    sumNums +=element
+  })
+  let digits=nums.join("").split("").map(Number)
+  digits.forEach(digit => {
+    sumDigits+=digit
+  });
+  return sumNums - sumDigits
+}
+
+console.log(differenceOfSum([1,15,6,3]))
+console.log(differenceOfSum([1,2,3,4]))
