884b172648f0ba4eaadf4b373a80704714fd2ab9
muguntha <muguntharamkumar1996@gmail.com>
2023-12-22 01:48:16 -0400

    solve leetcode problem divisorGame

--- challenges.js
+++ challenges.js
@@ -727,3 +727,50 @@ function arrayStringsAreEqual(word1,word2){
 console.log(arrayStringsAreEqual(["ab", "c"],["a", "bc"]))
 console.log(arrayStringsAreEqual(["a", "cb"],["ab", "c"]))
 console.log(arrayStringsAreEqual(["abc", "d", "defg"],["abcddefg"]))
+// Exercise:21 Alice and Bob take turns playing a game, with Alice starting first.
+
+// Initially, there is a number n on the chalkboard. On each player's turn, that player makes a move consisting of:
+
+// Choosing any x with 0 < x < n and n % x == 0.
+// Replacing the number n on the chalkboard with n - x.
+// Also, if a player cannot make a move, they lose the game.
+
+// Return true if and only if Alice wins the game, assuming both players play optimally.
+
+ 
+
+// Example 1:
+
+// Input: n = 2
+// Output: true
+// Explanation: Alice chooses 1, and Bob has no more moves.
+// Example 2:
+
+// Input: n = 3
+// Output: false
+// Explanation: Alice chooses 1, Bob chooses 1, and Alice has no more moves.
+function divisorGame(n){
+  return n%2==0 ? true : false
+}
+console.log(divisorGame(2))
+console.log(divisorGame(3))
+// Example :22 Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,0,1]
+// Output: 2
+// Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+// Example 2:
+
+// Input: nums = [0,1]
+// Output: 2
+// Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+// Example 3:
+
+// Input: nums = [9,6,4,2,3,5,7,0,1]
+// Output: 8
+// Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+ 
