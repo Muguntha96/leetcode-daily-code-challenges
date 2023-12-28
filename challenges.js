d24ea20cc2533ddf81355db9131cccb19e7a23a9
muguntha <muguntharamkumar1996@gmail.com>
2023-12-28 18:37:45 -0400

    solve leetode isSameAfterReversals problem

--- challenges.js
+++ challenges.js
@@ -1177,3 +1177,60 @@ function countDigits(num){
 console.log(countDigits(7))
 console.log(countDigits(121))
 console.log(countDigits(1248))
+
+// Exercise:32Reversing an integer means to reverse all its digits.
+
+// For example, reversing 2021 gives 1202. Reversing 12300 gives 321 as the leading zeros are not retained.
+// Given an integer num, reverse num to get reversed1, then reverse reversed1 to get reversed2. Return true if reversed2 equals num. Otherwise return false.
+
+ 
+
+// Example 1:
+
+// Input: num = 526
+// Output: true
+// Explanation: Reverse num to get 625, then reverse 625 to get 526, which equals num.
+// Example 2:
+
+// Input: num = 1800
+// Output: false
+// Explanation: Reverse num to get 81, then reverse 81 to get 18, which does not equal num.
+// Example 3:
+
+// Input: num = 0
+// Output: true
+// Explanation: Reverse num to get 0, then reverse 0 to get 0, which equals num.
+function isSameAfterReversals(num){
+  let arr=num.toString().split("")
+  if(num==0){
+    return true
+  }
+  if(arr[arr.length-1]==0){
+    return false
+  }else{
+    return true
+  }
+
+}
+console.log(isSameAfterReversals(526))
+console.log(isSameAfterReversals(1800))
+console.log(isSameAfterReversals(0))
+
+// Exercise:33Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+// Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+// Example 1:
+
+// Input: x = 123
+// Output: 321
+// Example 2:
+
+// Input: x = -123
+// Output: -321
+// Example 3:
+
+// Input: x = 120
+// Output: 21
