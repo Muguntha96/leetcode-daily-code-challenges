794e73db51ffc50187f30a701b5370a28d82fa53
muguntha <muguntharamkumar1996@gmail.com>
2024-01-19 12:57:56 -0400

    solve max product of array

--- challenges.js
+++ challenges.js
@@ -1305,3 +1305,31 @@ function minMovesToSeat(seats,students){
 // console.log(minMovesToSeat([4,1,5,9],[1,3,2,6]))
 // console.log(minMovesToSeat([3,1,5],[2,7,4]))
 console.log(minMovesToSeat([3,20,17,2,12,15,17,4,15,20],[10,13,14,15,5,2,3,14,3,18]))
+
+// Exercise:35 Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
+ 
+
+// Example 1:
+
+// Input: nums = [3,4,5,2]
+// Output: 12 
+// Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12. 
+// Example 2:
+
+// Input: nums = [1,5,4,5]
+// Output: 16
+// Explanation: Choosing the indices i=1 and j=3 (indexed from 0), you will get the maximum value of (5-1)*(5-1) = 16.
+// Example 3:
+
+// Input: nums = [3,7]
+// Output: 12
+
+function maxProduct(nums){
+
+  nums.sort((a,b)=>a-b)
+  return(nums[nums.length-2]-1)*(nums[nums.length-1]-1)
+   
+}
+console.log(maxProduct([3,4,5,2]))
+console.log(maxProduct([1,5,4,5]))
+console.log(maxProduct([3,7]))
