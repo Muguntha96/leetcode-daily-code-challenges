3d6783ed304c3bff4910a97ebe158300482a9348
muguntha <muguntharamkumar1996@gmail.com>
2024-01-25 15:53:34 -0400

    solve single number leetcode problem

--- challenges.js
+++ challenges.js
@@ -1375,3 +1375,42 @@ function dominanIndex(nums){
 console.log(dominanIndex([3,6,1,0]))
 console.log(dominanIndex([1,2,3,4]))
 console.log(dominanIndex([0,0,0,1]))
+
+// Exercise:37 Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,2,1]
+// Output: 1
+// Example 2:
+
+// Input: nums = [4,1,2,1,2]
+// Output: 4
+// Example 3:
+
+// Input: nums = [1]
+// Output: 1
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 3 * 104
+// -3 * 104 <= nums[i] <= 3 * 104
+// Each element in the array appears twice except for one element which appears only once.
+
+function singleNumber(nums){
+  nums.sort((a,b)=>a-b)
+  for(let i=0;i<nums.length;i=i+2){
+    if(nums[i]!==nums[i+1]){
+      return nums[i]
+    }
+  }
+}
+
+console.log(singleNumber([2,2,1]))
+console.log(singleNumber([4,1,2,1,2]))
+console.log(singleNumber([1]))
