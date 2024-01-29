45fddda212e52568e50759e3fee6ae026e3e5ba7
muguntha <muguntharamkumar1996@gmail.com>
2024-01-29 18:54:43 -0400

    solve isPalindrome leetcode problem

--- challenges.js
+++ challenges.js
@@ -1415,3 +1415,46 @@ function singleNumber(nums){
 console.log(singleNumber([2,2,1]))
 console.log(singleNumber([4,1,2,1,2]))
 console.log(singleNumber([1]))
+
+// Exercise :38 A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+// Example 2:
+
+// Input: s = "race a car"
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
+// Example 3:
+
+// Input: s = " "
+// Output: true
+// Explanation: s is an empty string "" after removing non-alphanumeric characters.
+// Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 2 * 105
+// s consists only of printable ASCII characters.
+function isPalindrome(s){
+  let str=s.replace(/[^A-Z0-9]/ig, "").toLowerCase()
+
+  for(let i=0;i<str.length;i++){
+    if(str[i]!=str[str.length-i-1]){
+      return false
+    }
+  }
+  return true
+}
+console.log(isPalindrome("A man, a plan, a canal: Panama"))
+console.log(isPalindrome("race a car"))
+console.log(isPalindrome(" "))
+// isPalindrome("race a car")
