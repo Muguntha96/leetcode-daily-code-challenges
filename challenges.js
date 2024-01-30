d030551ef8acae9574caeeb080eb052d9ecbb654
muguntha <muguntharamkumar1996@gmail.com>
2024-01-30 15:23:10 -0400

    solve countegments leetcode problem

--- challenges.js
+++ challenges.js
@@ -1557,3 +1557,41 @@ for(let i=0;i<tArr.length;i++){
 
 console.log(findTheDifference("abcd","abcde"))
 console.log(findTheDifference("","y"))
+
+// Exercise : 41 Given a string s, return the number of segments in the string.
+
+// A segment is defined to be a contiguous sequence of non-space characters.
+
+ 
+
+// Example 1:
+
+// Input: s = "Hello, my name is John"
+// Output: 5
+// Explanation: The five segments are ["Hello,", "my", "name", "is", "John"]
+// Example 2:
+
+// Input: s = "Hello"
+// Output: 1
+ 
+
+// Constraints:
+
+// 0 <= s.length <= 300
+// s consists of lowercase and uppercase English letters, digits, or one of the following characters "!@#$%^&*()_+-=',.:".
+// The only space character in s is ' '.
+
+function countSegments(s){
+  let count=0
+    let arr=s.split(" ")
+    for(let i=0;i<arr.length;i++){
+      if(arr[i]!==''){
+        count ++
+      }
+    }
+  return count
+}
+
+console.log(countSegments("Hello, my name is John"))
+console.log(countSegments("Hello"))
+console.log(countSegments(""))
