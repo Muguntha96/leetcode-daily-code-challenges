5fce0d63983f882521d4b5dcb51147a0c19a9aac
muguntha <muguntharamkumar1996@gmail.com>
2024-02-03 00:02:06 -0400

    solve leetcode firstUnique proble

--- challenges.js
+++ challenges.js
@@ -1717,3 +1717,39 @@ console.log(moveZeroes([0,1,0,3,12]))
 console.log(moveZeroes([0]))
 console.log(moveZeroes([0,1]))
 console.log(moveZeroes([0,0,1]))
+
+// Exercise:46 Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+ 
+
+// Example 1:
+
+// Input: s = "leetcode"
+// Output: 0
+// Example 2:
+
+// Input: s = "loveleetcode"
+// Output: 2
+// Example 3:
+
+// Input: s = "aabb"
+// Output: -1
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s consists of only lowercase English letters.
+
+function firstUnique(s){
+for(let i=0;i<s.length;i++){
+  if(s.indexOf(s[i])===s.lastIndexOf(s[i])){
+    return i
+  }
+  }
+  return -1
+}
+
+console.log(firstUnique("leetcode"))
+console.log(firstUnique("loveleetcode"))
+console.log(firstUnique("aabb"))
