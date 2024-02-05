e6d07c46306950fc7bfddd3eb51062da9303d3df
muguntha <muguntharamkumar1996@gmail.com>
2024-02-05 16:40:50 -0400

    solve leetcode wordPattern problem

--- challenges.js
+++ challenges.js
@@ -1785,3 +1785,53 @@ function majorityElement(nums){
 }
 console.log(majorityElement([3,2,3]))
 console.log(majorityElement([2,2,1,1,1,2,2]))
+
+// Exercise :48 Given a pattern and a string s, find if s follows the same pattern.
+
+// Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+
+ 
+
+// Example 1:
+
+// Input: pattern = "abba", s = "dog cat cat dog"
+// Output: true
+// Example 2:
+
+// Input: pattern = "abba", s = "dog cat cat fish"
+// Output: false
+// Example 3:
+
+// Input: pattern = "aaaa", s = "dog cat cat dog"
+// Output: false
+ 
+
+// Constraints:
+
+// 1 <= pattern.length <= 300
+// pattern contains only lower-case English letters.
+// 1 <= s.length <= 3000
+// s contains only lowercase English letters and spaces ' '.
+// s does not contain any leading or trailing spaces.
+// All the words in s are separated by a single space.
+
+function wordPattern(pattern,s){
+  let sArr=s.split(" ")
+  let map= new Map()
+  if(sArr.length !=pattern.length){
+    return false
+  }
+  if(new Set(sArr).size!=new Set(pattern).size){
+    return false
+  }
+  for(let i=0;i<pattern.length;i++){
+    if(map.has(pattern[i])&&map.get(pattern[i])!=sArr[i]){
+      return false
+    }
+    map.set(pattern[i],sArr[i])
+  }
+  return true
+}
+console.log(wordPattern( "abba","dog cat cat dog"))
+console.log(wordPattern("abba","dog cat cat fish"))
+console.log(wordPattern("aaaa","dog cat cat dog"))
