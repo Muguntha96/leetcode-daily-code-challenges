d9bfc900fd1a5f25a1f2c1bb0e005fdeb566879b
muguntha <muguntharamkumar1996@gmail.com>
2024-02-06 03:43:09 -0400

    solve firstPalindrome problem in leetcode

--- challenges.js
+++ challenges.js
@@ -1955,3 +1955,40 @@ function sumOfSquares(nums){
 }
 console.log(sumOfSquares([1,2,3,4]))
 console.log(sumOfSquares([2,7,1,19,18,3]))
+
+// Exercise : 51 Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an empty string "".
+
+// A string is palindromic if it reads the same forward and backward.
+
+ 
+
+// Example 1:
+
+// Input: words = ["abc","car","ada","racecar","cool"]
+// Output: "ada"
+// Explanation: The first string that is palindromic is "ada".
+// Note that "racecar" is also palindromic, but it is not the first.
+// Example 2:
+
+// Input: words = ["notapalindrome","racecar"]
+// Output: "racecar"
+// Explanation: The first and only string that is palindromic is "racecar".
+// Example 3:
+
+// Input: words = ["def","ghi"]
+// Output: ""
+// Explanation: There are no palindromic strings, so the empty string is returned.
+
+function firstPalindrome(words){
+  for(let i=0;i<words.length;i++){
+    let word=words[i].split('').reverse().join('')
+    if(word===words[i]){
+      return word
+    }
+  
+  }
+  return ''
+}
+console.log(firstPalindrome(["abc","car","ada","racecar","cool"]))
+console.log(firstPalindrome(["notapalindrome","racecar"]))
+console.log(firstPalindrome(["def","ghi"]))
