a18d054c67d42cb9c4ce28d9a31fd96956a8d81b
muguntha <muguntharamkumar1996@gmail.com>
2024-02-06 02:34:38 -0400

    solve sortPeople leetcode problem

--- challenges.js
+++ challenges.js
@@ -1835,3 +1835,51 @@ function wordPattern(pattern,s){
 console.log(wordPattern( "abba","dog cat cat dog"))
 console.log(wordPattern("abba","dog cat cat fish"))
 console.log(wordPattern("aaaa","dog cat cat dog"))
+
+// Example 49:You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
+
+// For each index i, names[i] and heights[i] denote the name and height of the ith person.
+
+// Return names sorted in descending order by the people's heights.
+
+ 
+
+// Example 1:
+
+// Input: names = ["Mary","John","Emma"], heights = [180,165,170]
+// Output: ["Mary","Emma","John"]
+// Explanation: Mary is the tallest, followed by Emma and John.
+// Example 2:
+
+// Input: names = ["Alice","Bob","Bob"], heights = [155,185,150]
+// Output: ["Bob","Alice","Bob"]
+// Explanation: The first Bob is the tallest, followed by Alice and the second Bob.
+ 
+
+// Constraints:
+
+// n == names.length == heights.length
+// 1 <= n <= 103
+// 1 <= names[i].length <= 20
+// 1 <= heights[i] <= 105
+// names[i] consists of lower and upper case English letters.
+// All the values of heights are distinct.
+function sortPeople(names,heights){
+  let sortArr=[]
+  let newArr=[]
+  for(let i=0;i<heights.length;i++){
+    let obj={
+      name :names[i],
+      heights : heights[i]
+    }
+    sortArr.push(obj)
+  }
+  sortArr.sort((a,b) => b.heights-a.heights)
+  for(let j=0;j<sortArr.length;j++){
+    newArr[j]=sortArr[j].name
+  }
+  return newArr
+}
+
+console.log(sortPeople(["Mary","John","Emma"],[180,165,170]))
+console.log(sortPeople(["Alice","Bob","Bob"],[155,185,150]))
