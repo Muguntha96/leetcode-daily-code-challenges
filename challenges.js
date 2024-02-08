ddcaf649adecc690122eb626d17b33c270d49c51
muguntha <muguntharamkumar1996@gmail.com>
2024-02-08 16:19:22 -0400

    solve leetcode sumOfUnique problem

--- challenges.js
+++ challenges.js
@@ -2099,3 +2099,45 @@ function uniqueOccurrences(arr){
 }
 console.log(uniqueOccurrences([1,2,2,1,1,3]))
 console.log(uniqueOccurrences([1,2]))
+
+// Exercise : 54 You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
+
+// Return the sum of all the unique elements of nums.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,2]
+// Output: 4
+// Explanation: The unique elements are [1,3], and the sum is 4.
+// Example 2:
+
+// Input: nums = [1,1,1,1,1]
+// Output: 0
+// Explanation: There are no unique elements, and the sum is 0.
+// Example 3:
+
+// Input: nums = [1,2,3,4,5]
+// Output: 15
+// Explanation: The unique elements are [1,2,3,4,5], and the sum is 15.
+
+function sumOfUnique(nums){
+  let map=new Map()
+  let sum=0
+  for(let i=0;i<nums.length;i++){
+    if(map.has(nums[i])){
+      map.set(nums[i],map.get(nums[i])+1)
+    }else{
+      map.set(nums[i],1)
+    }
+  }
+  for (let [key,value] of map){
+    if(value===1){
+      sum = sum + key
+    }
+  }
+  return sum
+}
+console.log(sumOfUnique([1,2,3,2]))
+console.log(sumOfUnique([1,1,1,1,1]))
