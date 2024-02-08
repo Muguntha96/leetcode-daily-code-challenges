33e09cc57dd627a4c4fb17b2016c28a5fb876a20
muguntha <muguntharamkumar1996@gmail.com>
2024-02-07 23:43:48 -0400

    solve leetcode problem findDifference

--- challenges.js
+++ challenges.js
@@ -1992,3 +1992,71 @@ function firstPalindrome(words){
 console.log(firstPalindrome(["abc","car","ada","racecar","cool"]))
 console.log(firstPalindrome(["notapalindrome","racecar"]))
 console.log(firstPalindrome(["def","ghi"]))
+
+// Exercise 52:Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+// Example 1:
+
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+// Example 2:
+
+// Input: strs = [""]
+// Output: [[""]]
+// Example 3:
+
+// Input: strs = ["a"]
+// Output: [["a"]]
+
+// function groupAnagrams(strs){
+//   let anagrams=[]
+//   strs.forEach(element => {
+    
+//   });
+
+// }
+// Exercise :52 Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+
+// answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+// answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+// Note that the integers in the lists may be returned in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums1 = [1,2,3], nums2 = [2,4,6]
+// Output: [[1,3],[4,6]]
+// Explanation:
+// For nums1, nums1[1] = 2 is present at index 0 of nums2, whereas nums1[0] = 1 and nums1[2] = 3 are not present in nums2. Therefore, answer[0] = [1,3].
+// For nums2, nums2[0] = 2 is present at index 1 of nums1, whereas nums2[1] = 4 and nums2[2] = 6 are not present in nums2. Therefore, answer[1] = [4,6].
+// Example 2:
+
+// Input: nums1 = [1,2,3,3], nums2 = [1,1,2,2]
+// Output: [[3],[]]
+// Explanation:
+// For nums1, nums1[2] and nums1[3] are not present in nums2. Since nums1[2] == nums1[3], their value is only included once and answer[0] = [3].
+// Every integer in nums2 is present in nums1. Therefore, answer[1] = [].
+
+function findDifference(nums1,nums2){
+let answer=[[],[]]
+nums1=[...new Set(nums1)]
+nums2=[... new Set(nums2)]
+for (let val of nums1){
+  if(!nums2.includes((val))){
+    answer[0].push(val)
+  }
+}
+for (let val of nums2){
+  if(!nums1.includes((val))){
+    answer[1].push(val)
+  }
+} 
+return answer
+} 
+console.log(findDifference([1,2,3],[2,4,6]))
+console.log(findDifference([1,2,3,3],[1,1,2,2]))
