397826537b2492ac1f2137d43ebe1b3d326bd132
muguntha <muguntharamkumar1996@gmail.com>
2024-02-08 16:07:41 -0400

    solve uniqueOccurrences problem in leetcode

--- challenges.js
+++ challenges.js
@@ -2060,3 +2060,42 @@ return answer
 } 
 console.log(findDifference([1,2,3],[2,4,6]))
 console.log(findDifference([1,2,3,3],[1,1,2,2]))
+
+// Exercise : 53 Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: arr = [1,2,2,1,1,3]
+// Output: true
+// Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+// Example 2:
+
+// Input: arr = [1,2]
+// Output: false
+// Example 3:
+
+// Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+// Output: true
+function uniqueOccurrences(arr){
+  let map= new Map()
+  let set=new Set()
+  for(let i=0;i<arr.length;i++){
+    if(map.has(arr[i])){
+      map.set(arr[i],map.get(arr[i])+1)
+    }else{
+      map.set(arr[i],1)
+    }
+  }
+  for(let val of map.values()){
+    set.add(val)
+  }
+    if(map.size===set.size){
+    return true
+  }else{
+    return false
+  }
+}
+console.log(uniqueOccurrences([1,2,2,1,1,3]))
+console.log(uniqueOccurrences([1,2]))
