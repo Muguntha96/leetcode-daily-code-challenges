25f732e6c71c8c702fbc22d2f2a5917e41f1be80
muguntha <muguntharamkumar1996@gmail.com>
2024-02-15 11:04:17 -0400

    solve leetcode fidMinOrMax problem

--- challenges.js
+++ challenges.js
@@ -2175,3 +2175,39 @@ function findNumbers(nums){
 }
 console.log(findNumbers([12,345,2,6,7896]))
 // findNumbers([12,345,2,6,7896])
+
+// Exercise:56 Given an integer array nums containing distinct positive integers, find and return any number from the array that is neither the minimum nor the maximum value in the array, or -1 if there is no such number.
+
+// Return the selected integer.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,2,1,4]
+// Output: 2
+// Explanation: In this example, the minimum value is 1 and the maximum value is 4. Therefore, either 2 or 3 can be valid answers.
+// Example 2:
+
+// Input: nums = [1,2]
+// Output: -1
+// Explanation: Since there is no number in nums that is neither the maximum nor the minimum, we cannot select a number that satisfies the given condition. Therefore, there is no answer.
+// Example 3:
+
+// Input: nums = [2,1,3]
+// Output: 2
+// Explanation: Since 2 is neither the maximum nor the minimum value in nums, it is the only valid answer. 
+function findNonMinOrMax(nums){
+  nums.sort((a,b)=>a-b)
+  let min=nums[0]
+  let max=nums[nums.length-1]
+  for(let i=0;i<nums.length;i++){
+    if(nums[i]!=min && nums[i]!=max){
+      return nums[i]
+    }
+  }
+  return -1
+}
+
+console.log(findNonMinOrMax([3,2,1,4]))
+console.log(findNonMinOrMax([1,2]))
