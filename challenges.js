638980d8e1bd67d2b5c96ef4267b5e11a10cd7d6
muguntha <muguntharamkumar1996@gmail.com>
2024-07-15 11:36:04 -0400

    solve leetcode duplicate problem

--- challenges.js
+++ challenges.js
@@ -2478,3 +2478,34 @@ function isValid(s){
 
 console.log(isValid("()[]{}"))
 console.log(isValid('(}]'))
+
+// Exercise : 74 Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,1], k = 3
+// Output: true
+// Example 2:
+
+// Input: nums = [1,0,1,1], k = 1
+// Output: true
+// Example 3:
+
+// Input: nums = [1,2,3,1,2,3], k = 2
+// Output: false
+function containsNearByDup(nums,k){
+  let map=new Map()
+  for(let i =0;i<nums.length;i++){
+    if(map.has(nums[i])){
+      if(Math.abs(i-map.get(nums[i]))<=k){
+        return true
+      }
+    }
+    map.set(nums[i],i)
+  }
+  return false
+}
+
+console.log(containsNearByDup([1,2,3,1],3))
