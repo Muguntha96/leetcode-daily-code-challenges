31a09c2d10514395e7e609e024bdc91dd9c53805
muguntha <muguntharamkumar1996@gmail.com>
2024-08-06 17:10:52 -0400

    solve sortSentence leetcode problem

--- challenges.js
+++ challenges.js
@@ -2509,3 +2509,37 @@ function containsNearByDup(nums,k){
 }
 
 console.log(containsNearByDup([1,2,3,1],3))
+
+// Exercise : 75 Sorting the Sentence
+// A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each word consists of lowercase and uppercase English letters.
+
+// A sentence can be shuffled by appending the 1-indexed word position to each word then rearranging the words in the sentence.
+
+// For example, the sentence "This is a sentence" can be shuffled as "sentence4 a3 is2 This1" or "is2 sentence4 This1 a3".
+// Given a shuffled sentence s containing no more than 9 words, reconstruct and return the original sentence.
+
+ 
+
+// Example 1:
+
+// Input: s = "is2 sentence4 This1 a3"
+// Output: "This is a sentence"
+// Explanation: Sort the words in s to their original positions "This1 is2 a3 sentence4", then remove the numbers.
+// Example 2:
+
+// Input: s = "Myself2 Me1 I4 and3"
+// Output: "Me Myself and I"
+// Explanation: Sort the words in s to their original positions "Me1 Myself2 and3 I4", then remove the numbers.
+
+function sortSentence(s){
+  let sArr=s.split(" ")
+sArr.sort((a,b)=>a[a.length-1]-b[b.length-1])
+  let senArr=[]
+  for(let i=0;i<sArr.length;i++){
+    senArr[i]=sArr[i].replace(/.$/, '')
+  }
+  return senArr.join(" ")
+}
+
+sortSentence("is2 sentence4 This1 a3")
+sortSentence("Myself2 Me1 I4 and3")
