41761aa81c479f86bef43f038c6f4166216f31e9
muguntha <muguntharamkumar1996@gmail.com>
2024-08-13 19:04:26 -0400

    solve minimumAverage

--- challenges.js
+++ challenges.js
@@ -2543,3 +2543,78 @@ sArr.sort((a,b)=>a[a.length-1]-b[b.length-1])
 
 sortSentence("is2 sentence4 This1 a3")
 sortSentence("Myself2 Me1 I4 and3")
+
+// Exercise : 76 Minimum Average of Smallest and Largest Elements
+// You have an array of floating point numbers averages which is initially empty. You are given an array nums of n integers where n is even.
+
+// You repeat the following procedure n / 2 times:
+
+// Remove the smallest element, minElement, and the largest element maxElement, from nums.
+// Add (minElement + maxElement) / 2 to averages.
+// Return the minimum element in averages.
+
+ 
+
+// Example 1:
+
+// Input: nums = [7,8,3,4,15,13,4,1]
+
+// Output: 5.5
+
+// Explanation:
+
+// step	nums	averages
+// 0	[7,8,3,4,15,13,4,1]	[]
+// 1	[7,8,3,4,13,4]	[8]
+// 2	[7,8,4,4]	[8,8]
+// 3	[7,4]	[8,8,6]
+// 4	[]	[8,8,6,5.5]
+// The smallest element of averages, 5.5, is returned.
+// Example 2:
+
+// Input: nums = [1,9,8,3,10,5]
+
+// Output: 5.5
+
+// Explanation:
+
+// step	nums	averages
+// 0	[1,9,8,3,10,5]	[]
+// 1	[9,8,3,5]	[5.5]
+// 2	[8,5]	[5.5,6]
+// 3	[]	[5.5,6,6.5]
+// Example 3:
+
+// Input: nums = [1,2,3,7,8,9]
+
+// Output: 5.0
+
+// Explanation:
+
+// step	nums	averages
+// 0	[1,2,3,7,8,9]	[]
+// 1	[2,3,7,8]	[5]
+// 2	[3,7]	[5,5]
+// 3	[]	[5,5,5]
+
+function minimumAverage(nums){
+let avgArr=[]
+let n=nums.length
+for(let i=0;i<n-1;i++){
+  if(nums[i]>nums[i+1]){
+    let temp=nums[i]
+    nums[i]=nums[i+1]
+    nums[i+1]=temp
+
+  }
+}
+console.log(nums)
+for(let j=0;j<n/2;j++){
+  let output=nums[j]+nums[n-1]/2
+  avgArr[j]=output
+}
+console.log(avgArr)
+
+}
+
+minimumAverage([7,8,3,4,15,13,4,1])
