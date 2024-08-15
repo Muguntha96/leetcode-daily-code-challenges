2d6af87ccc6d9123dcad613c345ab2e9702078fa
muguntha <muguntharamkumar1996@gmail.com>
2024-08-15 16:52:06 -0400

    solve destination city problem

--- challenges.js
+++ challenges.js
@@ -3,6 +3,8 @@
 // Example 1:Input: jewels = "aA", stones = "aAAbbbb" Output: 3
 // Example 2:Input: jewels = "z", stones = "ZZ" Output: 0
 
+const { set } = require("mongoose")
+
 function numJewelsInStone(jewels,stones){
   let count=0
   let arrStones=stones.split("")
@@ -2664,3 +2666,52 @@ return [...numsArr1,...numsArr2]
 
 
 console.log(sortArrayByParity([3,1,2,4]))
+
+// Exercise 78:1436. Destination City
+// Attempted
+// Easy
+// Topics
+// Companies
+// Hint
+// You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi. Return the destination city, that is, the city without any path outgoing to another city.
+
+// It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city.
+
+ 
+
+// Example 1:
+
+// Input: paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+// Output: "Sao Paulo" 
+// Explanation: Starting at "London" city you will reach "Sao Paulo" city which is the destination city. Your trip consist of: "London" -> "New York" -> "Lima" -> "Sao Paulo".
+// Example 2:
+
+// Input: paths = [["B","C"],["D","B"],["C","A"]]
+// Output: "A"
+// Explanation: All possible trips are: 
+// "D" -> "B" -> "C" -> "A". 
+// "B" -> "C" -> "A". 
+// "C" -> "A". 
+// "A". 
+// Clearly the destination city is "A".
+// Example 3:
+
+// Input: paths = [["A","Z"]]
+// Output: "Z"
+
+function destCity(paths){
+const source=new Set()
+let destination=[]
+for(const [src,dest] of paths){
+  source.add(src)
+  destination.push(dest)
+}
+for (const dest of destination){
+  if(!source.has(dest)){
+    return dest
+  }
+}
+}
+
+console.log(destCity([["B","C"],["D","B"],["C","A"]]))
+console.log(destCity([["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]))
