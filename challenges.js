7b038f6d218ad7659ac8d78aed2ddc113cfb0a3d
muguntha <muguntharamkumar1996@gmail.com>
2024-08-18 22:16:25 -0400

    solve reverseWords

--- challenges.js
+++ challenges.js
@@ -3134,3 +3134,52 @@ function countGoodTriplets(arr, a, b, c){
 
 // console.log(countGoodTriplets([3,0,1,1,9,7],7,2,3))
 console.log(countGoodTriplets([1,1,2,2,3],0,0,1))
+
+// Exercise :86 151. Reverse Words in a String
+// Medium
+// Topics
+// Companies
+// Given an input string s, reverse the order of the words.
+
+// A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+// Return a string of the words in reverse order concatenated by a single space.
+
+// Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+ 
+
+// Example 1:
+
+// Input: s = "the sky is blue"
+// Output: "blue is sky the"
+// Example 2:
+
+// Input: s = "  hello world  "
+// Output: "world hello"
+// Explanation: Your reversed string should not contain leading or trailing spaces.
+// Example 3:
+
+// Input: s = "a good   example"
+// Output: "example good a"
+// Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 104
+// s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+// There is at least one word in s.
+
+function reverseWords(s){
+let sArr=s.replace(/\s{2,}/g, ' ').trim().split(" ")
+let n=sArr.length-1
+for(let i=0;i<n/2;i++){
+  let temp=sArr[i]
+  sArr[i]=sArr[n-i]
+  sArr[n-i]=temp
+}
+return sArr.join(" ")
+}
+
+console.log(reverseWords("the sky is blue"))
