8da375c8206606777a9da1f95d481b4fe5f852df
muguntha <muguntharamkumar1996@gmail.com>
2024-08-18 22:03:24 -0400

    solve count good triplets

--- challenges.js
+++ challenges.js
@@ -3081,3 +3081,56 @@ return [answer1,answer2]
 
 console.log(findIntersectionValues([3,4,2,3],[1,5]))
 console.log(findIntersectionValues([4,3,2,3,1],[2,2,5,2,3,6]))
+
+// Exercise:85 1534. Count Good Triplets
+// Easy
+// Topics
+// Companies
+// Hint
+// Given an array of integers arr, and three integers a, b and c. You need to find the number of good triplets.
+
+// A triplet (arr[i], arr[j], arr[k]) is good if the following conditions are true:
+
+// 0 <= i < j < k < arr.length
+// |arr[i] - arr[j]| <= a
+// |arr[j] - arr[k]| <= b
+// |arr[i] - arr[k]| <= c
+// Where |x| denotes the absolute value of x.
+
+// Return the number of good triplets.
+
+ 
+
+// Example 1:
+
+// Input: arr = [3,0,1,1,9,7], a = 7, b = 2, c = 3
+// Output: 4
+// Explanation: There are 4 good triplets: [(3,0,1), (3,0,1), (3,1,1), (0,1,1)].
+// Example 2:
+
+// Input: arr = [1,1,2,2,3], a = 0, b = 0, c = 1
+// Output: 0
+// Explanation: No triplet satisfies all conditions.
+
+
+function countGoodTriplets(arr, a, b, c){
+  let count = 0
+  let n=arr.length
+  for(let i=0;i<n;i++){
+    for(let j =i+1;j<n;j++){
+      for(let k=j+1;k<n;k++){
+        if(Math.abs(arr[i] - arr[j]) <= a &&
+        Math.abs(arr[j] - arr[k]) <= b
+        && Math.abs(arr[i] - arr[k]) <= c){
+          count ++
+        }
+      }
+    }
+  }
+  
+  
+  return count;
+}
+
+// console.log(countGoodTriplets([3,0,1,1,9,7],7,2,3))
+console.log(countGoodTriplets([1,1,2,2,3],0,0,1))
