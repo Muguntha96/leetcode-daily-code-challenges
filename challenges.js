ad307a59a3a4a29a288e91ab026fb17315561753
muguntha <muguntharamkumar1996@gmail.com>
2024-08-20 00:07:06 -0400

    solve gcd of strings

--- challenges.js
+++ challenges.js
@@ -3284,3 +3284,51 @@ function mergeAlternately(words1,words2){
 }
 
 console.log(mergeAlternately("abc","pqr"))
+
+// Exercise:89 For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+
+// Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+ 
+
+// Example 1:
+
+// Input: str1 = "ABCABC", str2 = "ABC"
+// Output: "ABC"
+// Example 2:
+
+// Input: str1 = "ABABAB", str2 = "ABAB"
+// Output: "AB"
+// Example 3:
+
+// Input: str1 = "LEET", str2 = "CODE"
+// Output: ""
+ 
+
+// Constraints:
+
+// 1 <= str1.length, str2.length <= 1000
+// str1 and str2 consist of English uppercase letters.
+
+function gcdOfStrings(str1,str2){
+  let n=str1.length
+  let m=str2.length
+  let gcdStr=""
+  let result=Math.min(n,m)
+  while(result>0){
+    if(n%result==0 && m%result==0){
+      let subStr = str1.substring(0, result);
+            
+      if (str1 === subStr.repeat(n / result) && str2 === subStr.repeat(m / result)) {
+          return subStr
+    }
+    
+  }
+  result--
+}
+  return gcdStr
+
+}
+// console.log(gcdOfStrings("ABABAB","ABAB"))
+
+console.log(gcdOfStrings("ABCDEF","ABC"))
