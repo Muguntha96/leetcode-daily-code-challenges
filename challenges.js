bcf83141efa6499417ed962dae4748f9e951b1e9
muguntha <muguntharamkumar1996@gmail.com>
2024-08-20 20:35:01 -0400

    solve product except itself

--- challenges.js
+++ challenges.js
@@ -3493,3 +3493,61 @@ function canPlaceFlowers(flowerbed, n){
 // console.log(canPlaceFlowers([1,0,0,0,1],1))
 // console.log(canPlaceFlowers([1,0,0,0,1],2))
 console.log(canPlaceFlowers([1,0,0,0,0,0,1],2))
+
+
+// Exercise:93 238. Product of Array Except Self
+// Medium
+// Topics
+// Companies
+// Hint
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+// You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,4]
+// Output: [24,12,8,6]
+// Example 2:
+
+// Input: nums = [-1,1,0,-3,3]
+// Output: [0,0,9,0,0]
+ 
+
+// Constraints:
+
+// 2 <= nums.length <= 105
+// -30 <= nums[i] <= 30
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ 
+
+// Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
+
+function productExceptSelf(nums){
+  let n=nums.length
+  if(n===1){
+    return
+  }
+  let prefix=[]
+  let suffix=[]
+  let answer=[]
+  prefix[0]=1
+  for(let i=1;i<n;i++){
+    prefix[i]=prefix[i-1]*nums[i-1]
+  }
+  suffix[n-1]=1
+  for(let i=n-2;i>=0;i--){
+    suffix[i]=suffix[i+1]*nums[i+1]
+  }
+  for(let i=0;i<n;i++){
+    answer[i]=prefix[i]*suffix[i]
+  }
+  return answer
+}
+
+console.log(productExceptSelf([1,2,3,4]))
+console.log(productExceptSelf([-1,1,0,-3,3]))
